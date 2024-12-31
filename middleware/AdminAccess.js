fb4b26fb471a7ee3f225087c71ad8be229f1cd24
itaybiton <itaybit10@gmail.com>
2024-12-31 18:58:25 +0200

    typo

--- middleware/AdminAccess.js
+++ middleware/AdminAccess.js
@@ -1,5 +1,5 @@
 const checkAdminAccess = (req, res, next) => {
-    const adminSecret = req.haders['admin-secret']
+    const adminSecret = req.headers['admin-secret']
     if (adminSecret !== process.env.ADMIN_SECRET) {
       return res.status(403).json({ message: 'Forbidden: Unauthorized access' })
     }
