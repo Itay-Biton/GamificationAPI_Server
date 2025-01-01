77c35852f869017d6376d60ab5d7c8e93db678f1
itaybiton <itaybit10@gmail.com>
2025-01-01 20:35:06 +0200

    type missmatch

--- models/AppSchema.js
+++ models/AppSchema.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const AppSchema = new mongoose.Schema({
   appID: { type: String, required: true, unique: true },
   appName: { type: String, required: true },
-  players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-  achievements: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Achievement' }],
+  players: [{ type: String, ref: 'Player' }],
+  achievements: [{ type: String, ref: 'Achievement' }],
 });
 
 module.exports = mongoose.model('App', AppSchema);
